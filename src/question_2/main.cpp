76ebba148f2b1654f42f1648ac706698b4776bf7
quintinpeterson1 <157077856+quintinpeterson1@users.noreply.github.com>
2024-03-07 19:19:17 +0000

    question 2

--- src/question_2/main.cpp
+++ src/question_2/main.cpp
@@ -1,6 +1,32 @@
+#include <iostream>
+#include "question2.h"
+#include <limits> // Another great library
 
-int main()
-{
+using namespace std;
+
+int main() {
+    char choice;
+    int widgetSold;
+
+    do {
+        // Prompt
+        cout << "Enter the number of widgets sold: ";
+        while (!(cin >> widgetSold) || widgetSold < 1) {
+            cout << "Please enter a positive integer: ";
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+
+        int pointsEarned = get_earned_points(widgetSold);
+
+        cout << "Points Earned: " << pointsEarned << "\n";
+
+        cout << "Do you want to enter another value? (y/n): ";
+        cin >> choice;
+
+    } while (toupper(choice) == 'Y');
+
+    cout << "Program exited.\n";
 
     return 0;
 }
--- src/question_2/question2.cpp
+++ src/question_2/question2.cpp
@@ -4,3 +4,17 @@ bool test_config()
 {
     return true;
 }
+
+int get_earned_points(int sold) {
+    if (sold >= 1 && sold <= 5) {
+        return sold * 1;
+    } else if (sold >= 6 && sold <= 10) {
+        return 5 * sold;
+    } else if (sold >= 11 && sold <= 15) {
+        return 10 * sold;
+    } else if (sold >= 16) {
+        return 15 * sold;
+    } else {
+        return 0; 
+    }
+}
